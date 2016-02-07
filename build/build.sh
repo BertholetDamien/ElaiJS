0ab058b4335298fc68aa0700b21e49961423d403
Bertholet Damien <bertholet.damien@gmail.com>
2016-02-07 10:42:17 -0500

    Added License to build

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,8 @@ node r.js -o requireElaiJS-Build.js
 sed -i 's|,define("elaiJS/requireElaiJS",function(){});|;|g' requireElaiJS.min.js
 node r.js -o elaiJSPartial-Build.js
 node r.js -o elaiJSMain-Build.js
-cat requireElaiJS.min.js > elaiJS.min.js
+cat ../LICENSE > elaiJS.min.js
+cat requireElaiJS.min.js >> elaiJS.min.js
 cat elaiJSPartial.min.js >> elaiJS.min.js
 cat elaiJSMain.min.js >> elaiJS.min.js
 rm requireElaiJS.min.js
