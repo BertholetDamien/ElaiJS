3ca74807daafd137843dc33c5c464ec1cb041903
Bertholet Damien <bertholet.damien@gmail.com>
2016-02-09 07:57:22 -0500

    Fixed builder

--- build/elaiJSMain-Build.js
+++ build/elaiJSMain-Build.js
@@ -1,5 +1,22 @@
 ({
     baseUrl: "./../js/",
     include: ["elaiJS/elaiJSMain"],
+    exclude: [
+      "elaiJS/helper",
+      "elaiJS/binder",
+      "elaiJS/cascadeCaller",
+      "elaiJS/multicallback",
+      "elaiJS/propertiesManagerFactory",
+      "elaiJS/configuration",
+      "elaiJS/ressources",
+      "elaiJS/mode",
+      "elaiJS/navigator",
+      "elaiJS/webservice",
+      "elaiJS/defaultWebservices",
+      "elaiJS/defaultConfiguration",
+      "elaiJS/theme",
+      "elaiJS/language",
+      "elaiJS/localisation"
+    ],
     out: "elaiJSMain.min.js"
 })
