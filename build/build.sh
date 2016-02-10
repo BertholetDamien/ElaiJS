f81d11b77f4200094356a73d298253933327860d
Bertholet Damien <bertholet.damien@gmail.com>
2016-02-10 07:48:51 -0500

    Added version to ElaiJS build and build go into dist folder

--- build/build.sh
+++ build/build.sh
@@ -2,11 +2,18 @@ node r.js -o requireElaiJS-Build.js
 sed -i 's|,define("elaiJS/requireElaiJS",function(){});|;|g' requireElaiJS.min.js
 node r.js -o elaiJSPartial-Build.js
 node r.js -o elaiJSMain-Build.js
-cat ../LICENSE > elaiJS.min.js
+
+cp ../LICENSE LICENSE
+sed -i "s|{{version}}|$1|g" LICENSE
+
+cat LICENSE > elaiJS.min.js
 cat requireElaiJS.min.js >> elaiJS.min.js
 cat elaiJSPartial.min.js >> elaiJS.min.js
 cat elaiJSMain.min.js >> elaiJS.min.js
+
+rm LICENSE
 rm requireElaiJS.min.js
 rm elaiJSPartial.min.js
 rm elaiJSMain.min.js
-mv elaiJS.min.js ./../js/lib/elaiJS.min.js
+
+mv elaiJS.min.js ./../dist/elaiJS.min.js
