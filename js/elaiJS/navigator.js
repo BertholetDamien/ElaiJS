400a3b9f09320cb7a763338c47128843ba53595d
dbertholet <damien.sologlobe@sologlobe.com>
2016-09-05 11:38:45 -0400

    Added reload feature in navigator module

--- js/elaiJS/navigator.js
+++ js/elaiJS/navigator.js
@@ -8,7 +8,8 @@ define(['elaiJS/configuration', 'elaiJS/binder'],
   var EVENT = { beforeUnload: "beforeUnload",
 	              beforeUnloadInternal: "beforeUnloadInternal",
 	              beforeUnloadNavigator: "beforeUnloadNavigator",
-                pageChanged: "pageChanged"
+                pageChanged: "pageChanged",
+                reload: "reload"
   };
 	self.EVENT = EVENT;
 
@@ -69,6 +70,10 @@ define(['elaiJS/configuration', 'elaiJS/binder'],
 	self.back = function back() {
 		window.history.back();
 	};
+
+	self.reload = function reload() {
+		fire(EVENT.reload, currentPageInfo);
+	}
 	
 	self.getHistoryState = function getHistoryState(state) {
     return window.history.state;
